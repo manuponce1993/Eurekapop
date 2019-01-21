4ec3f78ab6d7c4bd90c4029387003e41cb7ba2da
Sebastian Canonaco <sebastiancanonaco@gmail.com>
2019-01-20 21:40:27 -0300

    modal

--- eureka01/assets/scripts/script.js
+++ eureka01/assets/scripts/script.js
@@ -63,7 +63,9 @@
     $( "a.scrollLink" ).click(function( event ) {
     event.preventDefault()
     $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500)
-    })
+     })
+
+
 
 })
 
